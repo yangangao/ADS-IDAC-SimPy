bb2417a6c351ac97d825765acc638b948c50dea8
jfzhang1985 <58840362+jfzhang1985@users.noreply.github.com>
2019-12-23 15:53:06 +0800

    replace something

--- src/sim_ship.py
+++ src/sim_ship.py
@@ -8,11 +8,7 @@ import matplotlib as plt
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 
-
-
-class Ship(object):
-
-    
+class Ship(object): 
     def __init__(self, env, mmsi, lon, lat, speed, heading):
         super().__init__()
         self.env       = env
@@ -23,7 +19,7 @@ class Ship(object):
         self.heading   = heading   #船艏向，°，正北方向为0，顺时针旋转为正
         
         self.RUDDER_MAX = 30       #最大操舵角度为30°
-        self.K         = 0.0579    #船舶旋回性指数
+        self.K         = 20    #船舶旋回性指数
         self.T         = 69.9784   #船舶追随性指数
         self.delta     = 0         #船舶当前时刻的操舵角度,向右为正，向左为负
         self.gama_old  = 0         #船舶上一时刻角速度
@@ -50,17 +46,19 @@ class Ship(object):
         while True:
             duration = 1 # 1s计算一次
             yield self.env.timeout(duration)
-            # time.sleep(1)
+#            time.sleep(1)
+            
+            self.update() #更新船舶运动参数
 
             # 现在假设shipspeed只有1和-1
             # 假设匀速运行, 计算对地速度
-            self.heading += self.gama
-            self.sog = self.speed * np.cos(self.heading) + sim_res.RIVER[self.lon, self.lat]
+#            self.heading += self.gama
+#            self.sog = self.speed * np.cos(self.heading * np.pi / 180) + sim_res.RIVER[self.lon, self.lat]
             # print('ship sog: %s' % self.sog)
             # 计算位置坐标
-            self.lon = (int)(self.lon + (self.sog * np.cos(self.heading)) * duration)
-            self.lat = (int)(self.lat + (self.sog * np.sin(self.heading)) * duration)
-            # print('np.cos(self.heading) : ', np.cos(self.heading))    
+#            self.lon = (self.lon + (self.speed * np.sin(self.heading * np.pi / 180)))
+#            self.lat = (self.lat + (self.speed * np.cos(self.heading * np.pi / 180)))
+#            print('ship heading: ', self.heading)    
 
             # print('time: %d' % env.now, 'mmsi: %s' % self.mmsi, 'lon: %d' %self.lon, 'lat: %d' % self.lat, )
             # 将每一个时刻的坐标存储下来
@@ -68,22 +66,16 @@ class Ship(object):
             # sim_res.SHIPINFO[mmsi+'lat'].append(self.lat)
 
             status_info = {'mmsi': self.mmsi, 'lon': self.lon, 'lat': self.lat, 'speed': self.speed, \
-            'heading': self.heading, 'sog': self.sog}
+            'heading': self.heading}
             sim_res.SHIPSTATUS.append(status_info) # 添加一条记录
             
     def TurnRight(self,):
         if self.delta <= self.RUDDER_MAX - 5:
             self.delta += 5
-        gama_temp = self.gama_old + (self.K * self.delta - self.gama_old) * self.T
-        self.gama_old = self.gama
-        self.gama = gama_temp
         
     def TurnLeft(self,):
         if self.delta >= -self.RUDDER_MAX + 5:
             self.delta -= 5
-        gama_temp = self.gama_old + (self.K * self.delta - self.gama_old) * self.T
-        self.gama_old = self.gama
-        self.gama = gama_temp
     
     #计算本船与目标船的DCPA    
     def ComputeDCPA(self, x_target, y_target, speed, heading):
@@ -159,13 +151,23 @@ class Ship(object):
             TCPA = d / (x**2+y**2)**0.5
         return TCPA
         
+    def update(self):
+        #更新船舶角速度
+        gama_temp = self.gama_old + (self.K * self.delta - self.gama_old) / self.T
+        self.gama_old = self.gama
+        self.gama = gama_temp
+        #更新船舶航向和位置
+        self.heading += self.gama
+        self.lon += self.speed * np.sin(self.heading * np.pi / 180)
+        self.lat += self.speed * np.cos(self.heading * np.pi / 180)
+        
     def change(self, speed, heading, ):
         '''
         人为主观因素干预下对船舶的调整，包括速度，方向等
         ----------
         shipspeed : 船舶自身的速度
         heading : 航向角
-        '''
+        '''     
         self.speed = speed
         self.heading = heading
         pass
